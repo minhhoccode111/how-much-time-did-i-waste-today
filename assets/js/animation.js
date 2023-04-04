e62e8809f57fe4ffbe8d1196b8ae4267ffc7968f
minhhoccode111 <minhhoccode111@gmail.com>
2023-04-04 14:22:46 +0700

    project's skeleton

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,1 +1,6 @@
 // console.log("Hello world from animation.js");
+
+//select elements
+
+// animation class
+class Animation {}
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
 // console.log("Hello world from app.js");
+
+// select html elements
+const submitBtn = document.getElementById("submit-button");
+
+//action and its information etc...
+class Action {}
+
+//event listener
+class Event {}
+
+//change UI
+class UI {}
+
+//storage
+class Storage {}
