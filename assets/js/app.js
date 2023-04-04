c91f5eb2a8ee2aceaa72d8cad71ee2eff9764be1
minhhoccode111 <minhhoccode111@gmail.com>
2023-04-04 23:09:06 +0700

    switch machine

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,30 @@
 // console.log("Hello world from app.js");
 
+//variables to use
+let actions = [];
+
 // select html elements
 const submitBtn = document.getElementById("submit-button");
+const form = document.querySelector("[data-form]");
 
 //action and its information etc...
 class Action {}
 
 //event listener
-class Event {}
+class Event {
+  static form() {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log("submit");
+    });
+  }
+}
 
 //change UI
 class UI {}
 
 //storage
 class Storage {}
+
+//call methods
+Event.form();
