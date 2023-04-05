cf736200580eda9c185518aa994a0f0099cd9723
minhhoccode111 <minhhoccode111@gmail.com>
2023-04-05 12:27:18 +0700

    finished adding action object after submit clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,49 @@
 // console.log("Hello world from app.js");
 
 //variables to use
-let actions = [];
+let todayActions = [];
+let today = {};
+let history = {};
 
 // select html elements
 const submitBtn = document.getElementById("submit-button");
 const form = document.querySelector("[data-form]");
+const buttons = document.querySelectorAll(".btn");
+const time = document.querySelector("#time");
+const actions = document.querySelectorAll('[name="Action"]');
 
 //action and its information etc...
-class Action {}
+class Action {
+  constructor(minutes, activity, today) {
+    this.minutes = minutes;
+    this.activity = activity;
+    this.today = today;
+  }
+}
 
 //event listener
 class Event {
   static form() {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
-      console.log("submit");
+      actions.forEach((action) => {
+        if (action.checked) {
+          let getTodayDate = getToday();
+          todayActions.push(
+            new Action(Number(time.textContent), action.value, getTodayDate)
+          );
+        }
+      });
+      console.log(todayActions);
+    });
+  }
+  static addMinutes() {
+    buttons.forEach((button) => {
+      button.addEventListener("mousedown", (e) => {
+        let timeValue = Number(time.textContent);
+        let addValue = Number(e.target.textContent);
+        time.innerHTML = timeValue + addValue;
+      });
     });
   }
 }
@@ -26,5 +54,32 @@ class UI {}
 //storage
 class Storage {}
 
+//functions for  hoisting
+function getToday() {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let month = date.getMonth();
+  month = months[month];
+  let day = date.getDate();
+  let year = date.getFullYear();
+  return `${hours}:${minutes} - ${year} ${month} ${day}`;
+}
+
 //call methods
 Event.form();
+Event.addMinutes();
